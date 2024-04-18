c0979ec6e172136c784b106e44d0bd0c58dcf21e
Sebastian Velez <sebasdj13@outlook.com>
2024-04-18 17:29:45 -0500

    Tests V1

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,6 +8,7 @@ import { getFirestore } from 'firebase/firestore/lite';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+//Dev/Prod
 const firebaseConfig = {
   apiKey: "AIzaSyCN6AycyP1Iz9BEZOc4Hp2Sd7qi1s6RSZo",
   authDomain: "journal-app-5572b.firebaseapp.com",
@@ -16,6 +17,16 @@ const firebaseConfig = {
   messagingSenderId: "441958173765",
   appId: "1:441958173765:web:9e63f3caf52b4e9ef5272c"
 };
+
+//Testing
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCDQbfzxt6iVrNHPBYggdTHX_Fvty2CR7o",
+//   authDomain: "react-testing-ea7d1.firebaseapp.com",
+//   projectId: "react-testing-ea7d1",
+//   storageBucket: "react-testing-ea7d1.appspot.com",
+//   messagingSenderId: "547661708239",
+//   appId: "1:547661708239:web:7def49c68cee5450b270ba"
+// };
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp( firebaseConfig );
